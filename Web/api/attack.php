64c280484c80ea5cf7a7b3b1002d660382e2a557
Matt Carter <m@ttcarter.com>
2011-08-20 08:22:51 +1000

    Added random testing data to Dexter tests

--- Web/api/attack.php
+++ Web/api/attack.php
@@ -8,17 +8,25 @@ if (defined($_GET['mac']))
 if (defined($_GET['name']))
 	die('No Name specified in GET');
 
+$do_random = 1; // Random output mode
+
 $result = array(
 	'ip' => $_GET['ip'],
-	'mac' => 'MAC',
-	'name' => 'NAME',
-	'ports' => array(
-		22 => 'SSH',
-		80 => 'Web',
-		666 => '',
-	),
-	'status' => 'warning',
+	'mac' => $_GET['mac'],
+	'name' => $_GET['name'],
+	'ports' => array(),
+	'status' => 'ok',
 );
 sleep(3);
+
+
+if ($do_random) {
+	if (rand(1,100) < 60) {
+		$result['status'] = 'warning';
+		foreach (range(1, rand(1,3)) as $offset)
+			$result['ports'][rand(50,300)] = 'ICMP';
+	}
+}
+
 echo json_encode($result);
 ?>
