a6df2c12bbb6fc63f4c9b245d7ef6891afb356a0
Matt Carter <m@ttcarter.com>
2011-08-20 08:28:51 +1000

    Actually made Dexter do something

--- Web/api/attack.php
+++ Web/api/attack.php
@@ -8,7 +8,8 @@ if (defined($_GET['mac']))
 if (defined($_GET['name']))
 	die('No Name specified in GET');
 
-$do_random = 1; // Random output mode
+$dexter = '/home/mc/Dexter/dexter'; // Path to dexter executable
+$do_random = 0; // Random output mode
 
 $result = array(
 	'ip' => $_GET['ip'],
@@ -20,12 +21,20 @@ $result = array(
 sleep(3);
 
 
-if ($do_random) {
+if ($do_random) { // Do a fake test
 	if (rand(1,100) < 60) {
 		$result['status'] = 'warning';
 		foreach (range(1, rand(1,3)) as $offset)
 			$result['ports'][rand(50,300)] = 'ICMP';
 	}
+} else { // Do an actual test
+	#$output = `perl $dexter $ip`;
+	$output = file_get_contents('dumps/report.dump');
+
+	preg_match_all('/Found open port ([0-9]+)/', $output, $matches, PREG_SET_ORDER);
+	$result['status'] = 'warning';
+	foreach ($matches as $port)
+		$result['ports'][$port[1]] = '';
 }
 
 echo json_encode($result);
--- Web/api/get_hosts.php
+++ Web/api/get_hosts.php
@@ -2,8 +2,8 @@
 // Simple script for parsing the output of bin/list into JSON
 
 $hosts = array();
-#$output = `perl bin/get_hosts.pl`;
-$output = file_get_contents('dumps/hosts.dump'); # FIXME: Debug
+$output = `perl bin/get_hosts.pl`;
+#$output = file_get_contents('dumps/hosts.dump'); # FIXME: Debug
 foreach (preg_split('/\n/', $output) as $line)
 	if (preg_match('/^(.+?),(.+?),(.*)$/', $line, $matches))
 		$hosts[] = array('ip' => $matches[1], 'mac' => $matches[2], 'name' => $matches[3]);
