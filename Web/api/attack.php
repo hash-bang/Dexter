aa563532ea09de0b063b9319442ae9652ebfd49c
Matt Carter <m@ttcarter.com>
2011-08-20 15:53:30 +1000

    BUGFIX: Various silly fixes

--- Web/api/attack.php
+++ Web/api/attack.php
@@ -9,7 +9,7 @@ if (defined($_GET['name']))
 	die('No Name specified in GET');
 
 $dexter = '/home/mc/Dexter/dexter'; // Path to dexter executable
-$do_random = 0; // Random output mode
+$do_fake = 0; // Random output mode (i.e. dont actually run a test)
 
 $result = array(
 	'ip' => $_GET['ip'],
@@ -18,23 +18,22 @@ $result = array(
 	'ports' => array(),
 	'status' => 'ok',
 );
-sleep(3);
 
-
-if ($do_random) { // Do a fake test
+if ($do_fake) { // Do a fake test
 	if (rand(1,100) < 60) {
 		$result['status'] = 'warning';
 		foreach (range(1, rand(1,3)) as $offset)
 			$result['ports'][rand(50,300)] = 'ICMP';
 	}
 } else { // Do an actual test
-	#$output = `perl $dexter $ip`;
-	$output = file_get_contents('dumps/report.dump');
+	$output = `perl $dexter '{$_GET['ip']}' --nocolor --network 127.0.0 2>&1`;
 
-	preg_match_all('/Found open port ([0-9]+)/', $output, $matches, PREG_SET_ORDER);
-	$result['status'] = 'warning';
-	foreach ($matches as $port)
-		$result['ports'][$port[1]] = '';
+	preg_match_all('/Found open port ([0-9]+)(?: \((.*?)\))?/', $output, $matches, PREG_SET_ORDER);
+	if ($matches) {
+		$result['status'] = 'warning';
+		foreach ($matches as $port)
+			$result['ports'][$port[1]] = ($port[2] ? $port[2] : '');
+	}
 }
 
 echo json_encode($result);
