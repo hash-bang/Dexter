0b03d5b7f49b28f7cc4ddc61c4c38f05d4c23a93
Matt Carter <m@ttcarter.com>
2011-08-20 15:52:40 +1000

    BUGFIX: Various

--- Web/api/get_hosts.php
+++ Web/api/get_hosts.php
@@ -7,5 +7,6 @@ $output = `perl bin/get_hosts.pl`;
 foreach (preg_split('/\n/', $output) as $line)
 	if (preg_match('/^(.+?),(.+?),(.*)$/', $line, $matches))
 		$hosts[] = array('ip' => $matches[1], 'mac' => $matches[2], 'name' => $matches[3]);
+#$hosts[] = array('ip' => '127.0.0.1', 'mac' => 'LOCALHOST', 'name' => 'LOCALHOST');
 echo json_encode($hosts);
 ?>
